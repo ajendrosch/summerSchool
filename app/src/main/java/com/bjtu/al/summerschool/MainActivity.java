0646a5c40fb1f50958811b6038c763be9a11ae9e
ajendrosch <alexander.jendrosch@rwth-aachen.de>
2016-06-28 15:06:27 +0200

    added Record/Play functionality Working???

--- app/src/main/java/com/bjtu/al/summerschool/MainActivity.java
+++ app/src/main/java/com/bjtu/al/summerschool/MainActivity.java
@@ -1,12 +1,86 @@
 package com.bjtu.al.summerschool;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.media.MediaRecorder;
+import android.media.MediaPlayer;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static String mFileName = null;
+
+    private ImageButton mRecordButton = null;
+    private MediaRecorder mRecorder = null;
+
+    private ImageButton mPlayButton = null;
+    private MediaPlayer mPlayer = null;
+
+    // saves State
+    boolean mStartRecording = true;
+    boolean mStartPlaying = true;
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,18 +89,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // record button
         imageButton = (ImageButton) findViewById(R.id.recordButton);
         imageButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View arg0) {
+                String showString = "Record button is clicked!";
+                onRecord(mStartRecording);
+                if (mStartRecording) {
+                    showString = "Stop recording";
+                } else {
+                    showString = "Start recording";
+                }
+                mStartRecording = !mStartRecording;
 
-                Toast.makeText(MainActivity.this,
-                        "ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, showString, Toast.LENGTH_SHORT).show();
 
             }
 
         });
+
+        // play button
+        imageButton = (ImageButton) findViewById(R.id.playButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                String showString = "Record button is clicked!";
+                onPlay(mStartPlaying);
+                if (mStartPlaying) {
+                    showString = "Stop playing";
+                } else {
+                    showString = "Start playing";
+                }
+                mStartPlaying = !mStartPlaying;
+
+                Toast.makeText(MainActivity.this, showString, Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
+
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
     }
 
 }
